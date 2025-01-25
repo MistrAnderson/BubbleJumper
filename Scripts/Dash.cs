8b8e04955f75c6fabe5fa997a1e292e22a104b35
malop <malo.poussardin@etu.univ-amu.fr>
2025-01-25 23:41:35 +0100

    feat: add dash

--- Scripts/Dash.cs
+++ Scripts/Dash.cs
@@ -1,21 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Dash : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float forceAmount = 50;
+    [SerializeField] Rigidbody2D rb;
+    public float speed = 10f;
+    public float mx;
+    public float dashDistance = 10f;
+    bool isDashing;
+    float doubleTapTime;
+    KeyCode LastKeyCode;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    public void dashRight()
+
+    private void Update()
     {
-        rb.AddForce(Vector2.right * forceAmount, ForceMode2D.Impulse);
+        //dash gauche
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            if (doubleTapTime > Time.time && LastKeyCode == KeyCode.A)
+            {
+                StartCoroutine(Dashs(-1));
+            }
+            else
+            {
+                doubleTapTime = Time.time + 0.3f;
+            }
+            LastKeyCode = KeyCode.A;
+        }
+        //dash droite
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            if (doubleTapTime > Time.time && LastKeyCode == KeyCode.D)
+            {
+                StartCoroutine(Dashs(1));
+            }
+            else
+            {
+                doubleTapTime = Time.time + 0.3f;
+            }
+            LastKeyCode = KeyCode.D;
+        }
     }
-    public void dashLeft()
+    private void FixedUpdate()
     {
-        rb.AddForce(Vector2.left * forceAmount, ForceMode2D.Impulse);
+        if (!isDashing)
+        {
+            rb.velocity = new Vector2(mx * speed, rb.velocity.y);
+        }
+    }
+    IEnumerator Dashs (float direction)
+    {
+        isDashing = true;
+        rb.velocity = new Vector2(rb.velocity.x, 0f);
+        rb.AddForce(new Vector2(dashDistance * direction, 0f), ForceMode2D.Impulse);
+        float gravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        yield return new WaitForSeconds(0.4f);
+        isDashing = false;
+        rb.gravityScale = gravity;
     }
 }
