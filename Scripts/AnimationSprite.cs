a5ab2fc3eb69488f7d9f3630d254090228e7649f
Nemo <nemo.muller@ynov.com>
2025-01-26 02:12:08 +0100

    feat: animation script

--- Scripts/AnimationSprite.cs
+++ Scripts/AnimationSprite.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+
+
 
 public class AnimationSprite : MonoBehaviour
 {
     // Start is called before the first frame update
+    float horizontalInput;
+    bool isFacingRight = false;
+    bool isGrounded = false;
+
+    Rigidbody2D rb;
+    Animator animator;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        FlipSprite();
+
+    }
+    private void FixedUpdate()
+    {
+        animator.SetFloat("xVelocity", Math.Abs(rb.velocity.x));
+        animator.SetFloat("yVelocity", rb.velocity.y);
+    }
+    void FlipSprite()
+    {
+        if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
     }
 }
